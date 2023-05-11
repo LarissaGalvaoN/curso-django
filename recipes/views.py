395b5db04d723aaa17825a53af889339480d4f3d
Larissa Galvão Nascimento <larissagalvao.nasc@gmail.com>
2023-05-11 10:56:01 -0300

    removed contact and about

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home (request):
     #busca o template na pasta template automaticamente
     return render(request, 'recipes/home.html')
 
-def contato (request):
-    return HttpResponse('Contato9')
 
-def sobre (request):
-    return HttpResponse('Sobre9')
